62b54354e9cc50591ac34f1ddaa1848f80270bf7
forgotting <forgotting@forgottingdeMacBook-Air.local>
2020-03-24 20:49:18 +0800

    modify title name

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
     <head>
-        <title>承德 - @yield('title')</title>
+        <title>丞德 - @yield('title')</title>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
