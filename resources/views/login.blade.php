379eb5dfb8ef90ac147e320b15603b9aad7ac30a
forgotting <forgotting@forgottingdeMacBook-Air.local>
2020-05-21 00:17:35 +0800

    modify alert's pwd with login page

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -2,7 +2,7 @@
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title">請輸入您的密碼</h5>
+                <h5 class="modal-title">請輸入您的密碼(1234)</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">×</span>
                 </button>
